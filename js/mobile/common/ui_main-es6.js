cc6f48e06dbec87e8f86dd459996d06c32459acd
pullthechin <pullthechin@gmail.com>
2023-08-27 06:27:37 +0900

    주말작업

---
+++ js/mobile/common/ui_main-es6.js
@@ -0,0 +1,65 @@
+(() => {
+  const swiperFn = (slide, indicator) => {
+    const rtValue = new Swiper(slide, {
+      slidesPerView: 1,
+      loop: true,
+      pagination: {
+        el: indicator,
+      },
+    });
+
+    return rtValue;
+  };
+
+  const recommendNewsSwiper = () => {
+    var recommendNews = swiperFn(
+      ".recommend-news-swiper",
+      ".recommend-news-indicator"
+    );
+  };
+
+  const newsNineSwiper = () => {
+    var newsNine = swiperFn(".news-9-swiper", ".news-9-indicator");
+  };
+
+  const kShortsSwiper = () => {
+    var kShorts = swiperFn(".k-shorts-swiper", ".k-shorts-indicator");
+  };
+
+  const youtubeLiveSwiper = () => {
+    var youtubeLive = swiperFn(
+      ".youtube-live-swiper",
+      ".youtube-live-indicator"
+    );
+  };
+
+  const tabClick = () => {
+    $(".tab-btn")
+      .off("click")
+      .on("click", function () {
+        var id = $(this).closest(".tab-btns").data("tab-contents");
+        var index = $(this).closest(".tab-btns-list").index();
+        $(this).closest(".tab-btns-list").siblings().removeClass("on");
+        $(this).closest(".tab-btns-list").addClass("on");
+        $("#" + id)
+          .find(".tab-contents-list")
+          .hide();
+        $("#" + id)
+          .find(".tab-contents-list")
+          .eq(index)
+          .show();
+      });
+  };
+
+  const init = () => {
+    recommendNewsSwiper();
+    newsNineSwiper();
+    kShortsSwiper();
+    youtubeLiveSwiper();
+    tabClick();
+  };
+
+  window.addEventListener("load", () => {
+    init();
+  });
+})();
---
+++ js/mobile/common/ui_main.js
@@ -0,0 +1,46 @@
+"use strict";
+
+(function () {
+  var swiperFn = function swiperFn(slide, indicator) {
+    var rtValue = new Swiper(slide, {
+      slidesPerView: 1,
+      loop: true,
+      pagination: {
+        el: indicator
+      }
+    });
+    return rtValue;
+  };
+  var recommendNewsSwiper = function recommendNewsSwiper() {
+    var recommendNews = swiperFn(".recommend-news-swiper", ".recommend-news-indicator");
+  };
+  var newsNineSwiper = function newsNineSwiper() {
+    var newsNine = swiperFn(".news-9-swiper", ".news-9-indicator");
+  };
+  var kShortsSwiper = function kShortsSwiper() {
+    var kShorts = swiperFn(".k-shorts-swiper", ".k-shorts-indicator");
+  };
+  var youtubeLiveSwiper = function youtubeLiveSwiper() {
+    var youtubeLive = swiperFn(".youtube-live-swiper", ".youtube-live-indicator");
+  };
+  var tabClick = function tabClick() {
+    $(".tab-btn").off("click").on("click", function () {
+      var id = $(this).closest(".tab-btns").data("tab-contents");
+      var index = $(this).closest(".tab-btns-list").index();
+      $(this).closest(".tab-btns-list").siblings().removeClass("on");
+      $(this).closest(".tab-btns-list").addClass("on");
+      $("#" + id).find(".tab-contents-list").hide();
+      $("#" + id).find(".tab-contents-list").eq(index).show();
+    });
+  };
+  var init = function init() {
+    recommendNewsSwiper();
+    newsNineSwiper();
+    kShortsSwiper();
+    youtubeLiveSwiper();
+    tabClick();
+  };
+  window.addEventListener("load", function () {
+    init();
+  });
+})();
